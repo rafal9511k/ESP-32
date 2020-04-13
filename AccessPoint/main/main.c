530e3ab11724ceeb8e37e8cddbc6a86d204d8077
root <root@rafal-Lenovo-G510>
2020-04-13 19:57:49 +0200

    create

---
+++ AccessPoint/main/main.c
@@ -0,0 +1,136 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "esp_netif.h"
+
+#include "print.h"
+
+#define AP_SSID		"esp32_AP"
+#define AP_PASS		"qwerty12345"
+
+void vWifiTask(void* pvParams);
+/**
+ * @brief Wifi event handler
+ *
+ * @param arg
+ * @param base_event
+ * @param event_id
+ * @param event_data
+ */
+void wifi_event_handler(
+		void* arg, esp_event_base_t base_event,
+		wifi_event_t event_id, void* event_data){
+	switch(event_id){
+	case WIFI_EVENT_AP_START:
+		printCF(COLOR_BLUE, "Access point started");
+		break;
+	case WIFI_EVENT_AP_STACONNECTED:
+	{
+		wifi_event_ap_staconnected_t* data
+		= (wifi_event_ap_staconnected_t*) event_data;
+		printCF(
+			COLOR_BLUE,
+			"STA connected to AP, MAC : "MACSTR"",
+			MAC2STR(data->mac)
+			);
+	}
+		break;
+	case WIFI_EVENT_AP_STADISCONNECTED:
+	{
+		wifi_event_ap_stadisconnected_t* data
+		= (wifi_event_ap_stadisconnected_t*) event_data;
+		printCF(
+			COLOR_BLUE,
+			"STA disconnected from AP, MAC : "MACSTR"",
+			MAC2STR(data->mac)
+			);
+	}
+		break;
+	default:
+		printCF(COLOR_BLUE, "Wifi event id = %d", event_id);
+
+	}
+
+}
+
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+
+    ESP_ERROR_CHECK(esp_event_handler_register(
+    		WIFI_EVENT,
+			ESP_EVENT_ANY_ID,
+			(esp_event_handler_t)wifi_event_handler,
+			NULL));
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+
+    wifi_config_t ap_config = {
+    		.ap = {
+    				.ssid = AP_SSID,
+					.ssid_len = strlen(AP_SSID),
+					.password = AP_PASS,
+					.channel = 1,
+					.max_connection = 1,
+					.authmode = WIFI_AUTH_WPA_WPA2_PSK
+    		}
+    };
+
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_AP, &ap_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+
+    xTaskCreate(
+    		vWifiTask,
+			"Wifi Task",
+			4096,
+			NULL,
+			5,
+			NULL);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+
+    while (true) {
+
+    }
+}
+
+/**
+ * @brief Prints MAC and IP address
+ *
+ * @param pvParams
+ */
+void vWifiTask(void* pvParams){
+	wifi_sta_list_t wifi_sta;
+	esp_netif_sta_list_t netif_sta;
+	esp_netif_sta_info_t netif_info;
+	while(true){
+		if(esp_wifi_ap_get_sta_list(&wifi_sta) != ESP_OK){
+			printCF(COLOR_CYAN, "Get STA list error");
+		}else{
+			/* get all STA ip and mac addr */
+			esp_netif_get_sta_list(&wifi_sta, &netif_sta);
+			printCF(COLOR_CYAN, "Number of connected STA = %d", netif_sta.num);
+			for(uint8_t i = 0; i < netif_sta.num; i++){
+				netif_info = netif_sta.sta[i];
+				printCF(
+					COLOR_CYAN,
+					"Device %d. MAC : "MACSTR" , IP : %s ",
+					i,
+					MAC2STR(netif_info.mac),
+					ip4addr_ntoa(&netif_info.ip));
+			}
+		}
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
---
+++ AccessPoint/main/print.h
@@ -0,0 +1,29 @@
+/*
+ * print.h
+ *
+ *  Created on: 30 mar 2020
+ *      Author: root
+ */
+
+#ifndef MAIN_PRINT_H_
+#define MAIN_PRINT_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define COLOR_RED		"31"
+#define COLOR_GREEN		"32"
+#define COLOR_YELLOW	"33"
+#define COLOR_BLUE		"34"
+#define COLOR_MAGENTA	"35"
+#define COLOR_CYAN		"36"
+
+// Print text in color
+#define printC(COLOR, text, ...) printf("\033[0;" COLOR "m" text "\033[0m\n", ##__VA_ARGS__)
+
+// Print text in color with function name
+#define printCF(COLOR, text, ...) printf("\033[0;" COLOR "m" "%s" " : " text "\033[0m\n",__func__,  ##__VA_ARGS__)
+
+
+#endif /* MAIN_PRINT_H_ */
